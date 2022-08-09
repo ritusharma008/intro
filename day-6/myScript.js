d696cd3ce5c929b1de57c53277cd186a4b6ac7c8
ritusharma0008 <tanaksharma012@gmail.com>
2022-08-09 15:48:00 +0530

    first

--- day-6/myScript.js
+++ day-6/myScript.js
@@ -15,7 +15,7 @@ var dayFormat = "<br> Today Day: "+ week[date.getDay()];
 document.write(dayFormat);
 
 //Assignment 2
-document.body.style.border = "2px solid black";
+document.body.style.border = "2px solid green";
 document.body.style.margin = "30%";
 document.body.style.backgroundColor = "pink";
 document.body.style.fontSize = "20px";
