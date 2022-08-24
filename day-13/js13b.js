c13ed2d6a1c0185efc5c50f848a9a0549b0a19dc
ritusharma0008 <tanaksharma012@gmail.com>
2022-08-24 17:05:38 +0530

    commit

---
+++ day-13/js13b.js
@@ -0,0 +1,22 @@
+// var boxs = document.querySelector('.container');
+
+// function box() {
+//     setTimeout(() => {
+//         boxs.style.visibility="visible";
+//     },3000);
+// }
+
+function openWindow() {
+    var url = "./assgnmnt13b.html";
+    var windowSize = "height =600, width= 800"
+    var newWindow = window.open(url,"Typing page",windowSize);
+    newWindow.moveTo(450,100);
+}
+setTimeout(
+    function openWindow() {
+        var url = "./assgnmnt13b.html";
+        var windowSize = "height =600, width= 800"
+        var newWindow = window.open(url,"Typing page",windowSize);
+        newWindow.moveTo(450,100);
+    },8000
+)
