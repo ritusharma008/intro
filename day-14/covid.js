ce2b738ff4cee3ce443c2b934549d8fbe9810a67
ritusharma0008 <tanaksharma012@gmail.com>
2022-08-25 16:27:10 +0530

    first

--- day-14/covid.js
+++ day-14/covid.js
@@ -11,12 +11,12 @@ getApiData(api_url);
 
 function displayData(serverData) {
     console.log(serverData);
-    document.getElementById('name').innerText = ""+serverData.Countries[7].Country;
-    document.getElementById('newConfirmed').innerText = ""+serverData.Countries[77].NewConfirmed;
-    document.getElementById('totalConfirmed').innerText = ""+serverData.Countries[77].TotalConfirmed;
-    document.getElementById('newDeath').innerText = ""+serverData.Countries[77].NewDeaths;
-    document.getElementById('totalDeath').innerText = ""+serverData.Countries[77].TotalDeaths;
-    document.getElementById('reCover').innerText = ""+serverData.Countries[77].NewRecovered;
-    document.getElementById('totalRecover').innerText = ""+serverData.Countries[77].TotalRecovered;
-    document.getElementById('date').innerText = ""+serverData.Countries[77].Date;
+    document.getElementById('name').innerText = "Country : "+serverData.Countries[7].Country;
+    document.getElementById('newConfirmed').innerText = "New Confirmed Cases : "+serverData.Countries[77].NewConfirmed;
+    document.getElementById('totalConfirmed').innerText = "Total Confirmed : "+serverData.Countries[77].TotalConfirmed;
+    document.getElementById('newDeath').innerText = "New Deaths : "+serverData.Countries[77].NewDeaths;
+    document.getElementById('totalDeath').innerText = "tOtal Deaths : "+serverData.Countries[77].TotalDeaths;
+    document.getElementById('reCover').innerText = "Recovered Cases : "+serverData.Countries[77].NewRecovered;
+    document.getElementById('totalRecover').innerText = "Total Recover : "+serverData.Countries[77].TotalRecovered;
+    document.getElementById('date').innerText = "Date : "+serverData.Countries[77].Date;
 }
